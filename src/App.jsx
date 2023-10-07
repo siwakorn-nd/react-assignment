2d639019b839f49fac78cde75773579f4dd45da9
krystalemblems <siwakorn.nd@gmail.com>
2023-10-07 16:24:27 +0700

    change owner picture and fix website title

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
     <Layout>
       <section className='flex flex-col content-center flex-wrap my-5'>
         <article className='mb-5'>
-        <h2 className='text-5xl py-4'>Generation Thailand</h2>
-        <h2 className='text-4xl text-center'>React - Assignment</h2>
+        <h2 className='text-4xl text-center'>Admin Panel</h2>
         </article>
         <article>
         <button className='btn btn-active btn-accent mx-3'><a href={'/userhomepage'}>User Home Selector</a></button>
--- src/owner_page/Owner.jsx
+++ src/owner_page/Owner.jsx
@@ -1,12 +1,12 @@
 import Layout from "../Layout";
-import Wonyoung from "../assets/Jang_Wonyoung_Eleven_Promo_2.png"
+import Cat from "../assets/cat.jpg"
 
 const OwnerPage = () => {
     return (
         <Layout>
             <div className="flex flex-col flex-wrap m-9">
                 <h1 className="text-5xl mb-10 self-center">Earth - Group B - 41</h1>
-                <img src={Wonyoung} alt="wonyoung" className="w-96 h-64 self-center" />
+                <img src={Cat} alt="wonyoung" className="w-96 h-64 self-center" />
                 <p className="self-center m-5 text-lg">Short Biography</p>
                 <p className="text-center self-center w-2/3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus quaerat, accusantium tempore ut fugiat quasi, vero est nam dicta temporibus praesentium quibusdam nulla ipsam, in nesciunt totam asperiores? Possimus velit, deleniti incidunt, optio exercitationem suscipit nulla ipsa maxime voluptates quo, voluptatibus quaerat? Odit aut maiores eos vel quo a, iusto animi adipisci delectus eligendi aliquid necessitatibus illum laboriosam architecto? Natus corrupti, praesentium id sequi molestiae debitis, voluptatem officia omnis doloremque voluptatibus possimus fugiat voluptate architecto qui ratione consectetur. Odio perspiciatis voluptates ad quaerat pariatur blanditiis sit quia deleniti, ab minus ipsum ipsa voluptatem debitis veritatis omnis esse, ullam repellendus eius.</p>
             </div>
